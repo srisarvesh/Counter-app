de03e04462a8e1a3bcb0f067e4f3439cbb196ee3
Web_python_ML_developer <r.srisarvesh@gmail.com>
2020-11-01 10:34:16 +0530

    Adding First component

---
+++ src/components/counter.jsx
@@ -0,0 +1,12 @@
+import React,{ Component } from 'react';
+
+export default class Counter extends Component {
+    render() {
+        return (<div>
+            <h1>Hi its gopi</h1>
+            <button>Increment</button>
+            </div>
+            );
+    }
+
+}
