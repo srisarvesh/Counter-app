1a47e18fe5ba6d6f9f58bb2cd0847273b1f996a0
Web_python_ML_developer <r.srisarvesh@gmail.com>
2020-11-01 11:50:22 +0530

    Adding First Button component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,11 +2,13 @@ import React,{ Component } from 'react';
 
 export default class Counter extends Component {
     state={
-        count:0
+        count:1,
+        imageurl:'https://i.imgur.com/gf9qMMK.png'
     }
     render() {
         return (
             <div>
+               <img src={this.state.imageurl} alt='' />
                <span>{this.formatcount()}</span>
                <button>Increment</button>
             </div>
