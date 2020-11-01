f3ac56712fcd9a012957f2938261a241716cad7d
Web_python_ML_developer <r.srisarvesh@gmail.com>
2020-11-01 18:58:50 +0530

    Adding list items

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,17 +3,33 @@ import React,{ Component } from 'react';
 export default class Counter extends Component {
     state={
         count:5,
-        imageurl:'https://i.imgur.com/gf9qMMK.png'
+        imageurl:'https://i.imgur.com/gf9qMMK.png',
+        tags:['tag1','tag2']
     }
     styles={
         fontSize:20
     }
+    rendertags(){
+        if(this.state.tags.length===0){
+            return <p>There are no tags</p>;
+        }
+        
+    return (
+    <ul>
+        {
+        this.state.tags.map(Tag=><li key={Tag}>{Tag}</li>)
+        }
+    </ul>
+    );
+    
+}
     
     render() {
         return (
             <div>
                <span style={this.styles} className={this.g()}>{this.formatcount()}</span>
                <button className="btn btn-secondary btn-sm" style={{fontSize:20 }}>Increment</button>
+               {this.rendertags()};
             </div>
             );
     }
