9ab000a6ade22f63f69ee1d16bbbeb1f188cf12a
Web_python_ML_developer <r.srisarvesh@gmail.com>
2020-11-01 10:52:41 +0530

    Adding First Button component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,8 @@ import React,{ Component } from 'react';
 
 export default class Counter extends Component {
     render() {
-        return (<div>
+        return (
+            <div>
             <h1>Hi its gopi</h1>
             <button>Increment</button>
             </div>
